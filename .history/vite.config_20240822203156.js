86ced3e382f243e894fc67d13de96ffe2b1ab595
Adjeneg21 <adjeng21@gmail.com>
2024-08-22 20:35:19 +0100

    update

---
+++ .history/vite.config_20240822203156.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+# Stage 1: Build
+FROM node:18-alpine AS build
+
+# Set working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy all files
+COPY . .
+
+EXPOSE 8080
+
+# Build the application
+CMD ["npm", "run", "dev"]
+
---
+++ .history/vite.config_20240822203243.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const PORT = `${env.PORT ?? "8080"}`;
+  console.log(env.PORT);
+  return {
+    plugins: [react()],
+
+    preview: {
+      port: PORT,
+      strictPort: true,
+    },
+    server: {
+      port: PORT,
+      host: true,
+      origin: `http://0.0.0.0:${PORT}`,
+    },
+  };
+});
